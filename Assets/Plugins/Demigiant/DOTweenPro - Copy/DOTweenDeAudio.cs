ab514e60dd6320be2b82c76f1f7de25d46865a6b
Dominik Konik <38404029+RandomowyRandom@users.noreply.github.com>
2022-12-26 21:00:25 +0100

    DOTween import

---
+++ Assets/Plugins/Demigiant/DOTweenPro - Copy/DOTweenDeAudio.cs
@@ -0,0 +1,7 @@
+﻿// ========================================================================
+// Copyright (c) 2020 DreamStorm Studios Piotr Penar. All Rights Reserved.
+// Confidential and Proprietary - Protected under copyright and other laws.
+// ========================================================================
+
+#if false // MODULE_MARKER
+#endif
