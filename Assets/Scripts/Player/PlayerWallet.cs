bfc8d7c7a1c3da9d64e39b2b2d48aa71984431ad
RandomowyRandom <38404029+RandomowyRandom@users.noreply.github.com>
2023-01-08 17:34:06 +0100

    Player object (#4)
    * Added game scene, basic player health, wallet
    
    * Added energy to the player

---
+++ Assets/Scripts/Player/PlayerWallet.cs
@@ -0,0 +1,52 @@
+﻿using Mirror;
+using Scriptables.Player;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerWallet : NetworkBehaviour
+    {
+        [SerializeField]
+        private PlayerWalletStats _playerBaseWalletStats;
+        
+        [SyncVar]
+        private int _currentMoney;
+
+        public int MoneyAmount => _currentMoney;
+        
+        public void AddMoney(int amount)
+        {
+            CmdSetMoney(_currentMoney + amount);
+        }
+        
+        public void SubtractMoney(int amount)
+        {
+            CmdSetMoney(_currentMoney - amount);
+        }
+        
+        public bool CanAfford(int amount)
+        {
+            return _currentMoney >= amount;
+        }
+        
+        public void SetMoney(int amount)
+        {
+            CmdSetMoney(amount);
+        }
+        
+        public override void OnStartClient()
+        {
+            CmdSetMoney(_playerBaseWalletStats.StartingMoney);
+        }
+        
+        #region Networking
+
+        [Command]
+        private void CmdSetMoney(int money)
+        {
+            _currentMoney = money;
+        }
+        
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Scriptables/Player/PlayerWalletStats.cs
@@ -0,0 +1,13 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Scriptables.Player
+{
+    public class PlayerWalletStats : SerializedScriptableObject
+    {
+        [SerializeField]
+        private int _startingMoney;
+        
+        public int StartingMoney => _startingMoney;
+    }
+}
