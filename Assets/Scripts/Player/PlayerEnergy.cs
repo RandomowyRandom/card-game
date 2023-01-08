b94e468dc96014c30bc6e7090d0a921b9aae4d40
RandomZiom <dkonik10@gmail.com>
2023-01-08 17:27:24 +0100

    Added energy to the player

---
+++ Assets/Scripts/Player/PlayerEnergy.cs
@@ -0,0 +1,42 @@
+﻿using Mirror;
+using Scriptables.Player;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerEnergy : NetworkBehaviour
+    {
+        [SerializeField]
+        private PlayerEnergyStats _playerBaseEnergyStats;
+        
+        [SyncVar]
+        private int _currentEnergy;
+        
+        public int CurrentEnergy => _currentEnergy;
+
+        public override void OnStartClient()
+        {
+            CmdSetEnergy(_playerBaseEnergyStats.StartingEnergy);
+        }
+        
+        public void AddEnergy(int amount)
+        {
+            CmdSetEnergy(_currentEnergy + amount);
+        }
+        
+        public void RemoveEnergy(int amount)
+        {
+            CmdSetEnergy(_currentEnergy - amount);
+        }
+
+        #region Networking
+
+        [Command]
+        private void CmdSetEnergy(int energy)
+        {
+            _currentEnergy = energy;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Scriptables/Player/PlayerEnergyStats.cs
@@ -0,0 +1,18 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Scriptables.Player
+{
+    public class PlayerEnergyStats : SerializedScriptableObject
+    {
+        [SerializeField]
+        private int _startingEnergy;
+        
+        [SerializeField]
+        private int _maxEnergy;
+        
+        public int MaxEnergy => _maxEnergy;
+        
+        public int StartingEnergy => _startingEnergy;
+    }
+}
