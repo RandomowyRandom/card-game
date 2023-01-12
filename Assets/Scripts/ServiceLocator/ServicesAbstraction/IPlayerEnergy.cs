6d588878861490272eec95427ef9bc01702e06f5
RandomZiom <dkonik10@gmail.com>
2023-01-12 18:31:34 +0100

    Energy UI

---
+++ Assets/Scripts/ServiceLocator/ServicesAbstraction/IPlayerEnergy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ServiceLocator.ServicesAbstraction
+{
+    public interface IPlayerEnergy: IService
+    {
+        public event Action<int> OnEnergyChanged;
+        
+        public int MaxEnergy { get; }
+        
+        public void AddEnergy(int amount);
+        public void RemoveEnergy(int amount);
+        public void SetEnergy(int amount);
+    }
+}
