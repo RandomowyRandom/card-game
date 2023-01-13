44ea76cb330fa55a0c51ea4b1f751c4bff64fe74
RandomowyRandom <38404029+RandomowyRandom@users.noreply.github.com>
2023-01-13 14:31:15 +0100

    Card model, texture, controll script, icons for most cards (#12)

---
+++ Assets/Scripts/Cards/CardWorld.cs
@@ -0,0 +1,54 @@
+﻿using Deck;
+using JetBrains.Annotations;
+using Scriptables.Cards.Abstractions;
+using TMPro;
+using UnityEngine;
+
+namespace Cards
+{
+    public class CardWorld: MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text _cardName;
+        
+        [SerializeField]
+        private TMP_Text _cardDescription;
+        
+        [SerializeField]
+        private TMP_Text _cardCost;
+        
+        [SerializeField]
+        private SpriteRenderer _cardIcon;
+        
+        [Space]
+        
+        [SerializeField]
+        private CardDatabase _cardDatabase;
+        
+        public void SetCard(Card card)
+        {
+            _cardName.text = card.CardName;
+            _cardDescription.text = card.Description;
+            _cardCost.text = card.EnergyCost.ToString();
+            _cardIcon.sprite = card.CardSprite;
+        }
+
+        #region QC
+
+        [QFSW.QC.Command("dev-set-card")] [UsedImplicitly]
+        private void SetCardCommand(string cardKey)
+        {
+            var card = _cardDatabase.GetCardByKey(cardKey);
+
+            if (card == null)
+            {
+                Debug.LogError($"Card {cardKey} does not exist!");
+                return;
+            }
+            
+            SetCard(card);
+        }
+
+        #endregion
+    }
+}
