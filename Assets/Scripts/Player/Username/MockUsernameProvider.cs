7b6aab5e7b6975de9d0ea4b784098ff464af8528
RandomZiom <dkonik10@gmail.com>
2023-01-22 16:02:13 +0100

    UI and logic for setting username

---
+++ Assets/Scripts/Player/Username/MockUsernameProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Common.Networking.PlayerManagement;
+using Mirror;
+using TMPro;
+using UnityEngine;
+
+namespace Player.Username
+{
+    public class MockUsernameProvider: MonoBehaviour, IUsernameProvider
+    {
+        [SerializeField]
+        private TMP_InputField _inputField;
+
+        private void Awake()
+        {
+            if(ServiceLocator.ServiceLocator.Instance.IsRegistered<IUsernameProvider>())
+                return;
+            
+            ServiceLocator.ServiceLocator.Instance.Register<IUsernameProvider>(this);
+        }
+
+        public string GetUsername()
+        {
+            return _inputField.text;
+        }
+    }
+}
