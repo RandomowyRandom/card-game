e7572723eb95becf82d195265f1bb09b244571cf
RandomZiom <dkonik10@gmail.com>
2023-01-22 15:06:08 +0100

    New cards scriptables

---
+++ Assets/Scripts/Scriptables/Cards/Effects/CardDrawCardEffect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Player.Hand;
+using Scriptables.Cards.Abstractions;
+using ServiceLocator.ServicesAbstraction;
+using Sirenix.Serialization;
+
+namespace Scriptables.Cards.Effects
+{
+    [Serializable]
+    public class CardDrawCardEffect: ICardEffect
+    {
+        [field: OdinSerialize]
+        public ITargetProvider TargetProvider { get; }
+        
+        [field: OdinSerialize]
+        private int Amount { get; }
+        public void OnUse()
+        {
+            var targets = TargetProvider.GetTargets();
+
+            foreach (var target in targets)
+            {
+                var hand = target.GetComponent<IPlayerHand>();
+                var deck = ServiceLocator.ServiceLocator.Instance.Get<ICardDeck>();
+                
+                for (var i = 0; i < Amount; i++)
+                {
+                    var card = deck.DrawCard();
+                    hand.AddCard(card);
+                }
+            }
+        }
+    }
+}
